42e6330716ea2a0aae09f3221e02ea3e71d4db07
salieksieienko <serhii.alieksieienko@capgemini.com>
2026-08-17 14:56:24 +0300

    initial commit

---
+++ src/main/java/org/example/sddinventory/SddInventoryApplication.java
@@ -0,0 +1,13 @@
+package org.example.sddinventory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SddInventoryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SddInventoryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/sddinventory/SddInventoryApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.sddinventory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SddInventoryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
